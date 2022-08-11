d5f92195775b04d15ed63c0ff91b4f1635673dd6
김상은 <tkddmsrla@collabi.co.kr>
2022-08-12 01:02:15 +0900

    Gateway Actuator 적용, profile 테스트

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,7 +1,10 @@
 package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,4 +13,11 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+	/*
+	 * 클라이언트가 요청했던 trace정보가 메모리에 담겨서 /actuator/httptrace 호출시 제공됨
+	 */
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
+	}
 }
