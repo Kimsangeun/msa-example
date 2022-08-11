c5efd4bd5df3d3419737be7ba5a65e95a66987f7
김상은 <tkddmsrla@collabi.co.kr>
2022-08-11 23:22:07 +0900

    user-service - Config 정보 반영을 위해 Spring Boot Actuator 적용

--- user-service/src/main/java/com/example/userservice/security/WebSecurity.java
+++ user-service/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -28,9 +28,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
-        http.authorizeRequests()
-                .antMatchers("/**")
+        http.authorizeRequests().antMatchers("/**")
                 .permitAll()
 //                .hasIpAddress("192.168.0.8")
                 .and()
